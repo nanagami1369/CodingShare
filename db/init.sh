27d3ebd65ef84e7a57bc746ce310a6c4fcdb75c7
nanagami1369 <nanagami1369@gmail.com>
2021-05-10 15:00:17 +0900

    DBを初期化するシェルスクリプトを作成

---
+++ db/init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(
+    cd $(dirname $0)
+    pwd
+)
+mysql -u$DB_USER -h$DB_IP -P$DB_PORT -p$DB_PASSWORD <$SCRIPT_DIR/schema.sql
+mysql -u$DB_USER -h$DB_IP -P$DB_PORT -p$DB_PASSWORD <$SCRIPT_DIR/sample_data.sql
+mysql -u$DB_USER -h$DB_IP -P$DB_PORT -p$DB_PASSWORD -e "use $DB_NAME;SELECT * FROM user;SELECT * FROM admin"
