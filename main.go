6c6bd6fd8887851bd5fa0c49b75b0a17f6717d05
nanagami1369 <nanagami1369@gmail.com>
2021-05-11 13:19:40 +0900

    ログイン処理を追加

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/nanagami1369/CodingShare/model"
@@ -10,6 +11,10 @@ import (
 
 func main() {
 	sm := module.NewStupModule()
+	uam, err := sm.GetUserAccountModule()
+	if err != nil {
+		panic(err)
+	}
 	db, err := sm.OpenDB()
 	if err != nil {
 		panic(err)
@@ -18,6 +23,28 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello World",
+		})
+	})
+	router.POST("/login", func(c *gin.Context) {
+		loginRequest := &model.LoginRequest{}
+		if err := c.ShouldBindJSON(&loginRequest); err != nil {
+			// JSONが読み込めなかったらエラーを返す
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+		user, err := uam.Login(loginRequest.Email)
+		if err != nil {
+			// ログインに失敗したら失敗した事だけ伝える
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"message": "Authentication failure",
+			})
+			return
+		}
+		// ログイン成功
+		c.JSON(200, gin.H{
+			"message": "Login Ok Hello " + user.Name + "!",
 		})
 	})
 	router.Run(":8081")
