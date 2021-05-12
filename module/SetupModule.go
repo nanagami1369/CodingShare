40aca560f70bf2ddc99d0b19aab0cb129cfe1129
nanagami1369 <nanagami1369@gmail.com>
2021-05-12 15:55:04 +0900

    エラーで設定していない環境変数の名称が間違っていたのを修正

--- module/SetupModule.go
+++ module/SetupModule.go
@@ -53,11 +53,11 @@ func (sm *SetupModule) createConnectURLFromEnv() (connectURL string, err error) 
 	}
 	dbPort := os.Getenv("DB_PORT")
 	if dbPort == "" {
-		return "", errors.New("環境変数「DB_USER」が定義されていません")
+		return "", errors.New("環境変数「DB_PORT」が定義されていません")
 	}
 	dbName := os.Getenv("DB_NAME")
 	if dbName == "" {
-		return "", errors.New("環境変数「DB_USER」が定義されていません")
+		return "", errors.New("環境変数「DB_NAME」が定義されていません")
 	}
 	connectURL = dbUser + ":" + dbPassword + "@tcp(" + dbIp + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 	return connectURL, nil
