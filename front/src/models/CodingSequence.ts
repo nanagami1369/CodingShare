005be8e43a557e83dd79537a441c464ddb9ea511
nanagami1369 <nanagami1369@gmail.com>
2021-05-17 15:20:05 +0900

    JSONのparse時に問題があったため、getter setterを使わないクラスにした

--- front/src/models/CodingSequence.ts
+++ front/src/models/CodingSequence.ts
@@ -1,29 +1,17 @@
 import CodeMirror from 'codemirror'
 
 export class CodingSequence {
-  private _timestamp: number
-  private _changeData: CodeMirror.EditorChangeLinkedList
-  private _scrollInfo: CodeMirror.ScrollInfo
+  public timestamp: number
+  public changeData: CodeMirror.EditorChangeLinkedList
+  public scrollInfo: CodeMirror.ScrollInfo
 
   constructor(
     timestamp: number,
     changeData: CodeMirror.EditorChangeLinkedList,
     scrollInfo: CodeMirror.ScrollInfo
   ) {
-    this._timestamp = timestamp
-    this._changeData = changeData
-    this._scrollInfo = scrollInfo
-  }
-
-  public get time(): number {
-    return this._timestamp
-  }
-
-  public get changeData(): CodeMirror.EditorChangeLinkedList {
-    return this._changeData
-  }
-
-  public get scrollInfo(): CodeMirror.ScrollInfo {
-    return this._scrollInfo
+    this.timestamp = timestamp
+    this.changeData = changeData
+    this.scrollInfo = scrollInfo
   }
 }
