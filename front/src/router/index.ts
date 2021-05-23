8adfaf49e352cda9d679027cbe8a3db1f153fada
nanagami1369 <nanagami1369@gmail.com>
2021-05-23 23:28:11 +0900

    不要なコメントを削除

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -13,20 +13,12 @@ const routes: Array<RouteConfig> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/AboutPage.vue'),
+    component: () => import('../views/AboutPage.vue'),
   },
   {
     path: '/editor',
     name: 'Editor',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/EditorPage.vue'),
+    component: () => import('../views/EditorPage.vue'),
   },
   {
     path: '/player',
