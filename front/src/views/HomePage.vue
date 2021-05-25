627dddb4e420cf7a66f773f3fc284584a31cd992
nanagami1369 <nanagami1369@gmail.com>
2021-05-25 14:47:40 +0900

    パスを間違えていたのを修正

--- front/src/views/HomePage.vue
+++ front/src/views/HomePage.vue
@@ -11,8 +11,8 @@
       </p>
     </div>
     <div class="controll-panel">
-      <a href="/#/editor"><i class="fas fa-video"></i>録画画面へ</a>
-      <a href="/#/player"><i class="fas fa-file-video"></i>再生画面へ</a>
+      <a href="/editor"><i class="fas fa-video"></i>録画画面へ</a>
+      <a href="/player"><i class="fas fa-file-video"></i>再生画面へ</a>
     </div>
   </div>
 </template>
