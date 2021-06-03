1aa49acfa02349fa698f91b171e099189dd8a598
nanagami1369 <nanagami1369@gmail.com>
2021-06-03 11:19:33 +0900

    #71 エディターにフォーカスがあたっていない状態でもカーソルを表示できるようにした

--- front/src/App.vue
+++ front/src/App.vue
@@ -56,4 +56,24 @@ body {
 .CodeMirror-hint {
   font-size: 2em !important;
 }
+
+.CodeMirror:not(.CodeMirror-focused) .CodeMirror-cursors {
+  visibility: visible !important;
+}
+
+.CodeMirror:not(.CodeMirror-focused) .CodeMirror-cursor {
+  animation: flash 1s linear infinite;
+}
+
+@keyframes flash {
+  0%,
+  50% {
+    opacity: 1;
+  }
+
+  51%,
+  100% {
+    opacity: 0;
+  }
+}
 </style>
