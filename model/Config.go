e84421a4b67158887c2f87925c462d39400c0a18
nanagami1369 <nanagami1369@gmail.com>
2021-08-13 20:43:23 +0900

    環境変数からの設定ファイルの読み込み結果をstructで返すようにした

---
+++ model/Config.go
@@ -0,0 +1,9 @@
+package model
+
+type Config struct {
+	DBUser     string
+	DBPassword string
+	DBIp       string
+	DBPort     string
+	DBName     string
+}
--- module/SetupModule.go
+++ module/SetupModule.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nanagami1369/CodingShare/model"
 	"github.com/nanagami1369/CodingShare/repository"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -18,7 +19,11 @@ func NewStupModule() *SetupModule {
 }
 
 func (sm *SetupModule) GetUserAccountModule() (module UserAccountModule, err error) {
-	db, err := sm.openDB()
+	config, err := sm.readConfigFromEnv()
+	if err != nil {
+		return nil, err
+	}
+	db, err := sm.openDB(config)
 	if err != nil {
 		return nil, err
 	}
@@ -31,35 +36,38 @@ func (sm *SetupModule) GetRouter() (router *gin.Engine, err error) {
 	return router, nil
 }
 
-func (sm *SetupModule) openDB() (db *gorm.DB, err error) {
-	url, err := sm.createConnectURLFromEnv()
-	if err != nil {
-		return nil, err
-	}
-	return gorm.Open(mysql.Open(url), &gorm.Config{})
+func (sm *SetupModule) openDB(config *model.Config) (db *gorm.DB, err error) {
+	connectURL := config.DBUser + ":" + config.DBPassword + "@tcp(" + config.DBIp + ":" + config.DBPort + ")/" + config.DBName + "?charset=utf8mb4&parseTime=True&loc=Local"
+	return gorm.Open(mysql.Open(connectURL), &gorm.Config{})
 }
 
-func (sm *SetupModule) createConnectURLFromEnv() (connectURL string, err error) {
+func (sm *SetupModule) readConfigFromEnv() (config *model.Config, err error) {
 	dbUser := os.Getenv("DB_USER")
 	if dbUser == "" {
-		return "", errors.New("環境変数「DB_USER」が定義されていません")
+		return nil, errors.New("環境変数「DB_USER」が定義されていません")
 	}
 	dbPassword := os.Getenv("DB_PASSWORD")
 	if dbPassword == "" {
-		return "", errors.New("環境変数「DB_PASSWORD」が定義されていません")
+		return nil, errors.New("環境変数「DB_PASSWORD」が定義されていません")
 	}
 	dbIp := os.Getenv("DB_IP")
 	if dbIp == "" {
-		return "", errors.New("環境変数「DB_IP」が定義されていません")
+		return nil, errors.New("環境変数「DB_IP」が定義されていません")
 	}
 	dbPort := os.Getenv("DB_PORT")
 	if dbPort == "" {
-		return "", errors.New("環境変数「DB_PORT」が定義されていません")
+		return nil, errors.New("環境変数「DB_PORT」が定義されていません")
 	}
 	dbName := os.Getenv("DB_NAME")
 	if dbName == "" {
-		return "", errors.New("環境変数「DB_NAME」が定義されていません")
+		return nil, errors.New("環境変数「DB_NAME」が定義されていません")
 	}
-	connectURL = dbUser + ":" + dbPassword + "@tcp(" + dbIp + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
-	return connectURL, nil
+	config = &model.Config{
+		DBUser:     dbUser,
+		DBPassword: dbPassword,
+		DBIp:       dbIp,
+		DBPort:     dbPort,
+		DBName:     dbName,
+	}
+	return config, nil
 }
