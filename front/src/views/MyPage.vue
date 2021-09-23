02e50fbf21f3ef5f30ed9d31faf4cdd8096e316d
nanagami1369 <nanagami1369@gmail.com>
2021-09-23 17:56:11 +0900

    mypageのユーザー情報をvuexから取得するようにした

--- front/src/views/MyPage.vue
+++ front/src/views/MyPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="my-page">
-    <p>ようこそ {{ userName }} 様</p>
+    <p>ようこそ {{ userId }} 様</p>
     <button @click="logout">ログアウト</button>
   </div>
 </template>
@@ -8,16 +8,12 @@
 <script lang="ts">
 import Vue from 'vue'
 
-type DataType = {
-  userName: string
-}
-
 export default Vue.extend({
   name: 'MyPage',
-  data(): DataType {
-    return {
-      userName: '',
-    }
+  computed: {
+    userId() {
+      return this.$store.getters.userId
+    },
   },
   methods: {
     logout: async function () {
@@ -41,27 +37,6 @@ export default Vue.extend({
         alert((error as Error).message)
       }
     },
-  },
-  async created(): Promise<void> {
-    try {
-      const response = await fetch('/api/private//auth', {
-        method: 'POST',
-        mode: 'cors',
-        credentials: 'include',
-      })
-      if (response.status != 200) {
-        // 失敗時はエラーを表示
-        let message =
-          `message:${await response.text()}\n` +
-          `http status:${response.status} ${response.statusText}`
-        alert(message)
-        return
-      }
-      this.userName = (await response.json()).userId
-    } catch (error: unknown) {
-      // fetchの例外はエラーとして処理
-      alert((error as Error).message)
-    }
   },
 })
 </script>
