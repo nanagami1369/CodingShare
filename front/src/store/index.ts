2053ce8bcc71f16acbb778568ead405b08beaa67
nanagami1369 <nanagami1369@gmail.com>
2021-09-23 17:49:50 +0900

    認証情報をvuexで管理するようにした

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,11 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+export const store = new Vuex.Store({
+  state: {
+    userId: 'ログインしていません',
+  },
+  getters: {
+    userId: (state) => state.userId, // 2
+  },
+  mutations: {
+    setUserId(state, newUserId) {
+      state.userId = newUserId
+    },
+    resetUserId(state) {
+      state.userId = 'ログインしていません'
+    },
+  },
+  actions: {
+    setUserIdAction({ commit }, newUserId) {
+      commit('setUserId', newUserId)
+    },
+    resetUserIdAction({ commit }) {
+      commit('resetUserId')
+    },
+  },
   modules: {},
 })
