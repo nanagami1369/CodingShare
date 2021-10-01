c49d6f1a14c2408b9035f7072dbef587ea2e0c09
nanagami1369 <nanagami1369@gmail.com>
2021-10-01 20:18:58 +0900

    検索機能を作成

--- front/src/views/SearchPage.vue
+++ front/src/views/SearchPage.vue
@@ -6,32 +6,47 @@
         <span>検索</span><span>&nbsp;</span><FontAwesomeIcon icon="search" />
       </button>
     </div>
-    <article class="search-resurlt-list">
+    <div v-if="searchedVideos.length == 0" class="search-resurlt">
       {{ this.$route.query.q }}に一致する検索結果はありません
-    </article>
+    </div>
+    <div v-else>
+      <div class="search-ward-title">{{ this.$route.query.q }}の検索結果</div>
+      <article class="search-resurlt-list">
+        <Thumbnail
+          v-for="video in searchedVideos"
+          :key="video.header.videoId"
+          :video="video"
+        />
+      </article>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import { Video } from '@/models/Video'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Thumbnail from '@/components/Thumbnail.vue'
 import Vue from 'vue'
 
 library.add(faSearch)
 
 type DataType = {
   searchWord: string | (string | null)[]
+  searchedVideos: Video[]
 }
 
 export default Vue.extend({
   name: 'HomePage',
   components: {
     FontAwesomeIcon,
+    Thumbnail,
   },
   data(): DataType {
     return {
       searchWord: '',
+      searchedVideos: [],
     }
   },
   computed: {
@@ -49,8 +64,23 @@ export default Vue.extend({
       }
       this.$router.push({ path: '/search', query: { q: this.searchWord } })
     },
-    observerUrlDo: function (): void {
+    observerUrlDo: async function (): Promise<void> {
       this.searchWord = this.query
+      if (!this.searchWord) {
+        return
+      }
+      try {
+        var response = await fetch(`/api/searchvideo?q=${this.searchWord}`)
+        if (response.ok) {
+          this.searchedVideos = (await response.json()) as Video[]
+        } else {
+          alert(`通信エラー\n${await response.text()}`)
+        }
+      } catch (error: unknown) {
+        // 通信エラーの場合はアラートで表示
+        alert((error as Error).message)
+        return
+      }
     },
   },
   created(): void {
@@ -73,7 +103,8 @@ export default Vue.extend({
   flex: 0 0 40px;
   display: flex;
   flex-direction: row;
-  margin: 25px 25px;
+  margin: 0px 25px;
+  margin-top: 25px;
 }
 .search-box input[type='search'] {
   flex: 0 1 600px;
@@ -95,9 +126,16 @@ export default Vue.extend({
 .search-box button:active {
   background: #aaaaaa;
 }
-
+.search-ward-title {
+  flex: 0 0 auto;
+  text-align: left;
+  font-size: 1.5em;
+  margin: 10px 25px;
+}
 .search-resurlt-list {
   flex: 1 1 auto;
-  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  margin: 10px;
 }
 </style>
