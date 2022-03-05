696c07105a421d140293a132a7c2156214a042ab
nanagami1369 <nanagami1369@gmail.com>
2022-03-05 17:14:02 +0900

    ホーム画面を移植した

--- front-vue3/src/App.vue
+++ front-vue3/src/App.vue
@@ -1,124 +1,58 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div id="app">
+    <div id="nav">
+      <RouterLink to="/">Home</RouterLink> |
+      <RouterLink to="/about">About</RouterLink>
     </div>
-  </header>
+    <router-view />
+  </div>
 
   <RouterView />
 </template>
 
 <style>
+* {
+  margin: 0px;
+  padding: 0px;
+}
 
 #app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  margin-top: 2rem;
+  color: #2c3e50;
+  height: 100%;
+  display: grid;
+  grid-template-rows: 50px 1fr;
+  /* 画面がスクロールしないようにする */
+  overflow-x: hidden;
+  overflow-y: hidden;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+#nav {
+  padding: 15px;
+  grid-row: 1;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 
-nav a:first-of-type {
-  border: 0;
+html {
+  height: 100%;
 }
 
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+body {
+  height: 100%;
 }
 </style>
--- front-vue3/src/views/HomeView.vue
+++ front-vue3/src/views/HomeView.vue
@@ -1,9 +1,64 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFileVideo, faVideo, faFilm } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { RouterLink } from 'vue-router'
+
+library.add(faFileVideo, faVideo, faFilm)
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home-page">
+    <div class="logo-panel">
+      <h1 class="app-logo">CodingShare{}</h1>
+      <p>本サイトはベータ版です。</p>
+      <p>
+        本ソフトを使用したことによるいかなる損害について、開発者は何も保証しません。
+      </p>
+      <p>
+        これに同意できない場合、あなたは本ソフトを使用することができません。
+      </p>
+    </div>
+    <div class="controll-panel">
+      <!-- prettier-ignore -->
+      <RouterLink to="/editor"><FontAwesomeIcon icon="video" />録画画面へ</RouterLink>
+      <!-- prettier-ignore -->
+      <RouterLink to="/player"><FontAwesomeIcon icon="file-video" />再生画面へ</RouterLink>
+      <!-- prettier-ignore -->
+      <RouterLink to="/list"  ><FontAwesomeIcon icon="film" />動画一覧へ</RouterLink>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.app-logo {
+  font-size: 5em;
+}
+
+.home-page {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.controll-panel a {
+  text-decoration: none;
+  border-bottom: solid 2px black;
+  color: black;
+}
+.controll-panel a:hover {
+  color: #28e270;
+  border-bottom: solid 2px #28e270;
+}
+
+.controll-panel a:active {
+  color: #ff4500;
+  border-bottom: solid 2px #ff4500;
+}
+
+.controll-panel {
+  display: flex;
+  flex-direction: column;
+  font-size: 2em;
+}
+</style>
